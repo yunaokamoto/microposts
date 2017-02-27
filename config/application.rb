2f1ae96cdbd09c518d388cc31228f663660bdd23
yuna <yuna0702@gmail.com>
2017-02-27 20:49:56 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -24,3 +24,14 @@ module Microposts
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
+
+module Microposts
+  class Application < Rails::Application
+config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
